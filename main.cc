4a7ffd7eb46f9eeadfb21b65332f8ca090dc5a22
Lei Qiao <qiaol618@gmail.com>
2015-08-11 17:20:51 -0500

    read matrix from file and write it out

--- main.cc
+++ main.cc
@@ -1,16 +1,28 @@
 #include "MDFILU.h"
+#include <fstream>
 
-int main()
+int main (int argc, char *argv[])
 {
+  Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, ::numbers::invalid_unsigned_int);
   const unsigned int degree (10);
-  LA::MPI::SparseMatrix system_matrix (degree, degree);
-  DynamicMatrix LU (degree, degree);
+  LA::MPI::SparseMatrix system_matrix (degree, degree, /*max_entries_per_row*/degree);
+  DynamicMatrix LU (degree, degree, /*max_entries_per_row*/degree);
 
   // Set value for system_matrix
-
+  std::ifstream fin ("matrix.dat");
+  for (unsigned i=0; i<degree; ++i)
+    for (unsigned j=0; j<degree; ++j)
+      {
+        data_type value;
+        fin >> value;
+        system_matrix.set (i,j,value);
+      }
+  fin.close();
+  system_matrix.compress (VectorOperation::insert);
   // Out put system_matrix
-
-  MDF_reordering_and_ILU_factoring (system_matrix, LU);
+  std::ofstream fout ("matrix.out");
+  system_matrix.print (fout);
+  // MDF_reordering_and_ILU_factoring (system_matrix, LU);
 
   // Out put LU
 
