3bd238e2623e87cd465179f5a7b59b77c51f8d2e
Lei Qiao <qiaol618@gmail.com>
2015-08-11 22:31:09 -0500

    update postprocess tools

--- deal2MTX.cc
+++ deal2MTX.cc
@@ -1,14 +1,30 @@
 #include <fstream>
+#include <iostream>
+
+using namespace std;
 
 int main (int argc, char *argv[])
 {
+
+  if (argc<3)
+    {
+      cerr << "Please tell me the input and output file names in command line parameters."
+           << endl;
+      return (1);
+    }
 
   unsigned n_row (0);
   unsigned n_column (0);
   int n_none_zero (-1);
 
   {
-    std::ifstream fin ("matrix.out");
+    std::ifstream fin (argv[1]);
+    if (!fin.good())
+      {
+        cerr << "Cannot open the specified input file."
+             << endl;
+        return (2);
+      }
     while (!fin.eof())
       {
         ++n_none_zero;
@@ -25,11 +41,26 @@ int main (int argc, char *argv[])
   }
 
 
-  std::ofstream fout ("matrix.MTX");
+  std::ofstream fout (argv[2]);
+  if (!fout.good())
+    {
+      cerr << "Cannot open the specified output file."
+           << endl;
+      return (3);
+    }
+
   fout <<"%%MatrixMarket matrix coordinate real general\n";
   fout << n_row+1 << "\t" << n_column +1 << "\t"
        << n_none_zero << std::endl;
-  std::ifstream fin ("matrix.out");
+
+  std::ifstream fin (argv[1]);
+  if (!fin.good())
+    {
+      cerr << "Cannot open the specified input file for second time reading."
+           << endl;
+      return (4);
+    }
+
   for (int line=0; line < n_none_zero; ++line)
     {
       unsigned i,j;
